f2d37f02cb646ac623a8db3e19226c758bd1f379
Benjamin <86573978+bennie10colado@users.noreply.github.com>
2023-07-02 03:09:11 -0300

    Modelos de testes para medico e consultum

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+# consulta-sim/test/models/medico_test.rb
+
+require 'test_helper'
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "salvar medico com campos vazios" do
+    medico = Medico.new(nome_completo: '', CPF: '', email: '', especialidade: '', CRM: '')
+    assert_not medico.save, "Salvar medico sem dados (não foi salvo)"
+  end
+
+  test "nome completo do médico deve ter pelo menos 5 caracteres" do
+    medico = Medico.new(nome_completo: 'X')
+    assert_not medico.save, "Salvar medico com nome menor que 5 caracteres (não foi salvo)"
+  end
+
+  test "nome completo do médico não pode conter numeros ou caracteres especiais" do
+    medico = Medico.new(nome_completo: 'Jô Soares 123!')
+    assert_not medico.save, "Salvar medico com nome com numeros (não foi salvo)"
+  end
+
 end
